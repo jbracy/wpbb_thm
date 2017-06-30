1b0fa0b4b8bfb06515be59e8997b2284e298baba
jeb <watertuman@gmail.com>
2017-06-30 16:43:22 -0500

    home page template added

---
+++ page_home.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * @package Bracy
+ * @subpackage Blog Theme
+ * @since Blog Theme 1.0
+ */
+get_header(); ?>
+
+  <section class="small-12 columns">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <!-- page title-->
+
+    <header class="text-center">
+      <h1><?php the_title();?></h1>
+    </header>
+
+    <!-- end page title-->
+
+    <!-- main content -->
+    <div class="post">
+      <?php the_content(); ?>
+    </div>
+    <?php endwhile; else : ?>
+      <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
+    <!-- end main content -->
+  </section>
+  <!-- sidebar -->
+  <aside>
+    <div class="small-12 large-3 columns sidebar">
+      <?php get_sidebar('home-1');?>
+    </div>
+    <div class="small-12 large-3 columns sidebar">
+      <?php get_sidebar('home-2');?>
+    </div>
+    <div class="small-12 large-3 columns sidebar">
+      <?php get_sidebar('home-3');?>
+    </div>
+  </aside>
+  <!-- end sidebar -->
+<?
+  get_footer();
+?>
