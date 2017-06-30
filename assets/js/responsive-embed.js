ebcb25f89353f51d64faf7dcd025745266e36b85
jeb <watertuman@gmail.com>
2017-06-30 18:06:36 -0500

    fix responsive issues with video embed, updated home page template layout

--- assets/js/responsive-embed.js
+++ assets/js/responsive-embed.js
@@ -1,11 +1,8 @@
 jQuery(document).foundation();
 
-if (jQuery('iframe').parent().hasClass() == 'post') {
-  jQuery('iframe').wrap('<p class="responsive-embed"></p>');
-}
-else {
-  jQuery('iframe').parent().addClass('responsive-embed');
-}
+jQuery('iframe').each(function () {
+  jQuery(this).wrap('<div class="responsive-embed"></div>');
+});
 
 var imgWidth = jQuery('.gallery-icon img').css('width');
 jQuery('figure').css('width', imgWidth);
--- page_home.php
+++ page_home.php
@@ -10,13 +10,6 @@ get_header(); ?>
 
   <section class="small-12 columns">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <!-- page title-->
-
-    <header class="text-center">
-      <h1><?php the_title();?></h1>
-    </header>
-
-    <!-- end page title-->
 
     <!-- main content -->
     <div class="post">
