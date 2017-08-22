63eb720bf018bd9d222425c5091a76630f6e4b6b
jeb <watertuman@gmail.com>
2017-08-22 12:05:29 -0400

    Update version number & prep for distribution.

--- functions.php
+++ functions.php
@@ -142,7 +142,7 @@ function gps_bb_features()  {
   function gps_bb_scripts() {
     wp_enqueue_style( 'foundation', get_template_directory_uri() . '/assets/css/foundation.min.css');
     wp_enqueue_style( 'foundation-icons', get_template_directory_uri() . '/assets/css/foundation-icons/foundation-icons.css');
-		wp_enqueue_style( 'main', get_stylesheet_uri(), array(), '1.3.1');
+		wp_enqueue_style( 'main', get_stylesheet_uri(), array(), '1.4.0');
 		wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/js/vendor/foundation.min.js', array('jquery'), null, true);
     wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/app.js', array('jquery'), null, true);
 	}
